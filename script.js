c48c2fde4d5f757ffd36cfaebc545672dbdfc217
Dkluizz <luizmusician@gmail.com>
2024-02-07 12:07:08 -0300

    up JS

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('.nav');
 const navbar = document.querySelector('.navbar');
-let section = document.querySelectorAll('.section');
-let navLink = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('.section');
+const navLinks = document.querySelectorAll('.link');
 
+function setActiveNavLink(id) {
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+    document.querySelector(`header nav a[href="#${id}"]`).classList.add('active');
+}
 
-menu.addEventListener("click", ()=> nav.classList.toggle('active'));
-
-navbar.addEventListener("click", ()=> nav.classList.toggle('active'));
-
-window.addEventListener('scroll',()=>{
-    section.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop;
-        let heightSection = sec.offsetHeight-120;
-        let idSection = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + heightSection){
-            navLink.forEach(link => {
-                link.classList.remove('active')
-                document.querySelector(`header nav a[href*=${idSection}]`).classList.add('active')
-            })
+function updateActiveSection() {
+    const scrollY = window.scrollY;
+    sections.forEach(sec => {
+        const offset = sec.offsetTop;
+        const heightSection = sec.offsetHeight -112;
+        const idSection = sec.getAttribute('id');
+        if (scrollY >= offset && scrollY < offset + heightSection) {
+            setActiveNavLink(idSection);
         }
-    })
-})
+    });
+}
+
+menu.addEventListener('click', () => nav.classList.toggle('active'));
+navbar.addEventListener('click', () => nav.classList.toggle('active'));
+window.addEventListener('scroll', updateActiveSection);
+window.addEventListener('load', updateActiveSection);
+
+
+const header = document.querySelector('header');
+
+function toggleHeaderScrollClass() {
+    if (window.scrollY > 0) {
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+}
+
+window.addEventListener('scroll', toggleHeaderScrollClass);
+
 
